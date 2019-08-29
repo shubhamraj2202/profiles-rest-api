3fe1a2f19d52cdb966bded3dd620d13fe4675c2c
shubhamraj2202 <shubhamraj2202@gmail.com>
2019-08-29 11:50:08 +0530

    Test Hello APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+    """Test API view"""
+
+    def get(self, request, format=None):
+        """Returns list of API View features"""
+        api_view = [
+            "Uses method (post, get, put, patch, delete)",
+            "Similar to traditional django view",
+            "Gives control over logic",
+            "Is mapped to URLs",
+        ]
+
+        return Response({
+                "Message": "Hello",
+                "Api": api_view
+            })
