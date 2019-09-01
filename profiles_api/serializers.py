568231d9a8c3a2c36e14673cead37b6176ac0563
shubhamraj2202 <shubhamraj2202@gmail.com>
2019-09-01 13:12:09 +0530

    Added UserProfile Serializer/View/Restered URL

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,32 @@
 from rest_framework import serializers
+from . import models
 
 
 class HelloSerializer(serializers.Serializer):
     """Seriliazes a name field for testing API View"""
     name = serializers.CharField(max_length=15)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+    class Meta:
+        """Need a meta class to point to specific model"""
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {
+                    'input_type': 'password'
+                }
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and Return a New User"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password'],
+        )
+        return user
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from . import serializers
-
+from . import serializers, models
 
 
 class HelloAPIView(APIView):
@@ -94,3 +93,9 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle Creating and Updating Profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
